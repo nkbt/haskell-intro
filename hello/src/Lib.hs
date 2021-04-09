d62b587fb83ea62aa09952e560fd9b0213d23940
Nikita Butenko <nik@butenko.me>
2021-04-09 10:53:09 +1000

    Custom Typeclass and VScode installation info

--- hello/src/Lib.hs
+++ hello/src/Lib.hs
@@ -4,3 +4,37 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+type Name = String
+
+greet :: Name -> String
+greet n = "Hello, " ++ n
+
+isSameName :: Name -> Name -> Bool
+isSameName n1 n2 = n1 == n2
+
+
+data Status = Available | ClosingSoon | Closed | Exhausted
+
+data DeliveryWindow = DeliveryWindow {
+  name :: Name,
+  status :: Status
+} deriving (Eq, Show)
+
+
+instance Eq Status where
+   Available == Available       = True
+   ClosingSoon == ClosingSoon   = True
+   Closed == Closed             = True
+   Exhausted == Exhausted       = True
+   _ == _                       = False
+
+instance Show DeliveryWindow where show a = "DeliveryWindow (" ++ ")"
+
+instance Show Status where
+   Available = "Available"
+   ClosingSoon = "ClosingSoon"
+   Closed = "Closed"
+   Exhausted = "Exhausted"
+
